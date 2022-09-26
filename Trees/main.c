a602631fdf552d8bdb14f3a8a0ff8808154845ae
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-09-26 17:17:54 +1000

    Add behaviour tests of ADT in main.c

--- Trees/main.c
+++ Trees/main.c
@@ -1,3 +1,14 @@
+#include "BSTree.h"
+
 int main(int argc, char *argv[]) {
+    Tree t = TreeCreate();
+    t = TreeInsert(t, 4);
+    t = TreeInsert(t, 2);
+    t = TreeInsert(t, 6);
+    t = TreeInsert(t, 5);
+    t = TreeInsert(t, 1);
+    t = TreeInsert(t, 7);
+    t = TreeInsert(t, 3);
+    showTree(t);
     return 0;
 }
