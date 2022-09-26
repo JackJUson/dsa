87f080654d5c97f5d67a35545fca5ce3cff6b475
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-09-26 17:33:51 +1000

    Add pseudo code for TreeInsert function

--- Trees/BSTree.c
+++ Trees/BSTree.c
@@ -24,5 +24,17 @@ Tree TreeInsert(Tree t, Item it) {
     return NULL;
 }
 
+/*
+TreeInsert(Tree, item):
+    if Tree is empty:
+        return new root node containing item
+    else if item < Tree's value:
+        Tree's left child = TreeInsert(Tree's left child , item)
+    else if item > Tree's value:
+        Tree's right child = TreeInsert(Tree's right child, item)
+    else:
+        return tree (avoid duplicates)
+*/
+
 void showTree(Tree t) {
 }
