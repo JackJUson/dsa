fd1bf21c5303491da979dc1a71df718e7cb0d1f7
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-09-26 16:58:47 +1000

    Add implementation stub within BSTree.c

--- Trees/BSTree.c
+++ Trees/BSTree.c
@@ -6,3 +6,16 @@ typedef struct Node {
     Tree right;
 } Node;
 
+Tree TreeCreate() {
+    return NULL;
+}
+
+void TreeFree(Tree t) {
+}
+
+Tree TreeInsert(Tree t, Item it) {
+    return NULL;
+}
+
+void showTree(Tree t) {
+}
