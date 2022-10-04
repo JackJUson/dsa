9c5342891e20f8e3b059190aa0825e4b1f745ca2
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-04 23:38:05 +1100

    Written implementation for BSTreeNodeHeight function in BSTree.c

--- Trees/BSTree.c
+++ Trees/BSTree.c
@@ -180,6 +180,21 @@ int TreeHeight(Tree t) {
     }
 }
 
+/* Get the height of a specific node in tree */
+int BSTreeNodeHeight(Tree t, int key) {
+    if (t == NULL)
+        return -1;
+    else if (key == t->value)
+        return 0;
+    else if (key < t->value) {
+        int depth = BSTreeNodeHeight(t->left, key);
+        return (depth == -1 ? -1 : depth + 1);
+    } else {
+        int depth = BSTreeNodeHeight(t->right, key);
+        return (depth == -1 ? -1 : depth + 1);
+    }
+}
+
 /* Prints all elements of tree in List */
 void showTree(Tree t) {
     if (t != NULL) {
