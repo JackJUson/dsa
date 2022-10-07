60001092b455febfaf9fba110e0d817c113dbf79
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-07 15:35:26 +1100

    Move all BST print functions into BSTPrint.c

---
+++ Trees/BSTPrint.h
@@ -0,0 +1,10 @@
+
+#ifndef BSTPRINT_H
+#define BSTPRINT_H
+
+void showTree(Tree t);
+void showTreeIn(Tree t, int zero);
+void showTreePre(Tree t, int zero);
+void showTreePost(Tree t, int zero);
+
+#endif
