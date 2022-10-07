46d5a789d1a2e03e5f2da7479facd6cae828106f
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-07 16:18:16 +1100

    Add BSTree property functions into BSTProperty.c

---
+++ Trees/BSTProperty.c
@@ -0,0 +1,52 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "BSTree.h"
+#include "BSTProperty.h"
+
+/* Count number of nodes in Tree */
+Item TreeNumNodes(Tree t) {
+    if (t == NULL) {
+        return 0;
+    } else {
+        return 1 + TreeNumNodes(left(t)) + TreeNumNodes(right(t));
+    }
+}
+
+/* Get the height of a tree */
+int TreeHeight(Tree t) {
+    if (t == NULL)
+        return -1;
+    else {
+        int l = TreeHeight(t->left);
+        int r = TreeHeight(t->right);
+        return 1 + (l > r ? l : r);
+    }
+}
+
+/* Get the smallest element of tree */
+Tree BSTreeGetSmallest(Tree t) {
+    if (t == NULL)
+        return NULL;
+    else if (t->left == NULL)
+        return t;
+    else
+        return BSTreeGetSmallest(t->left);
+}
+
+/* Get the height of a specific node in tree */
+int BSTreeNodeHeight(Tree t, int key) {
+    if (t == NULL)
+        return -1;
+    else if (key == data(t))
+        return 0;
+    else if (key < data(t)) {
+        int depth = BSTreeNodeHeight(t->left, key);
+        return (depth == -1 ? -1 : depth + 1);
+    } else {
+        int depth = BSTreeNodeHeight(t->right, key);
+        return (depth == -1 ? -1 : depth + 1);
+    }
+}
---
+++ Trees/BSTProperty.h
@@ -0,0 +1,10 @@
+
+#ifndef BSTPROPERTY_H
+#define BSTPROPERTY_H
+
+Item TreeNumNodes(Tree t);
+int TreeHeight(Tree t);
+Tree BSTreeGetSmallest(Tree t);
+int BSTreeNodeHeight(Tree t, int key);
+
+#endif
