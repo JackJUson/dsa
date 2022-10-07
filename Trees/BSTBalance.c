edc070c8a26139e89471e79e1a93f76d4a9d0249
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-07 16:03:16 +1100

    Add all BST balance functions in BSTBalance.c

---
+++ Trees/BSTBalance.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "BSTree.h"
+#include "BSTBalance.h"
+
+/* Rotating tree to the right */
+Tree rotateRight(Tree n1) {
+    if (n1 == NULL || left(n1) == NULL)
+        return n1;
+    Tree n2 = left(n1);
+    left(n1) = right(n2);
+    right(n2) = n1;
+    return n2;
+}
+
+/* Rotating tree to the left */
+Tree rotateLeft(Tree n1) {
+    if (n1 == NULL || right(n1) == NULL)
+        return n1;
+    Tree n2 = right(n1);
+    right(n1) = left(n2);
+    left(n2) = n1;
+    return n2;
+}
+
+/* Inserting at the root */
+/* And rotating the tree to make the new node the new root */
+Tree insertAtRoot(Tree t, Item it) {
+    Tree root = t;
+    if (t == NULL) {
+        t = TreeCreate(it);
+    } else if (it < data(root)) {
+        left(t) = insertAtRoot(left(t), it);
+        t = rotateRight(t);
+    } else if (it > data(root)) {
+        right(t) = insertAtRoot(right(t), it);
+        t = rotateLeft(t);
+    }
+    return t;
+}
+
+/* Partitioning Tree */
+/* Rearranging tree to make node with index i becomes root */
+/* Index of node are based on pre-order, smallest -> biggest */
+Tree partition(Tree t, int index) {
+    if (t != NULL) {
+        assert(0 <= index && index < TreeNumNodes(t));
+        int m = TreeNumNodes(left(t));
+        if (index < m) {
+            left(t) = partition(left(t), index);
+            t = rotateRight(t);
+        } else if (index > m) {
+            right(t) = partition(right(t), index - m - 1);
+            t = rotateLeft(t);
+        }
+    }
+    return t;
+}
---
+++ Trees/BSTBalance.h
@@ -0,0 +1,10 @@
+
+#ifndef BSTBALANCE_H
+#define BSTBALANCE_H
+
+Tree rotateRight(Tree n1);
+Tree rotateLeft(Tree n1);
+Tree insertAtRoot(Tree t, Item it);
+Tree partition(Tree t, int index);
+
+#endif
