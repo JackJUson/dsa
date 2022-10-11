a63e4b2beb4c428c5980e0870109f8c45e3d5d72
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-11 22:10:37 +1100

    Written implementation for balancing steps in insertAVL

--- Trees/AVL/AVLtree.c
+++ Trees/AVL/AVLtree.c
@@ -20,7 +20,34 @@ Tree insertAVL(Tree t, Item it) {
     t->height = updateHeight(t);
 
     /* 3. Get the balance factor of this ancestor
-          node to check whether this node is unbalanced */
+          node to check whether this node is unbalanced. 
+          balance = difference in height of left and right */
     int balance = getBalance(t);
 
+    /* 4. If tree node is unbalanced, then there are 4 cases */
+
+    // Left Left Case
+    // Left subtree is bigger than right subtree by 1
+    if (balance > 1 && it < t->left->data)
+        return rightRotate(t);
+
+    // Right Right Case
+    // Right subtree is bigger than left subtree by 1
+    if (balance < -1 && it > t->right->data)
+        return leftRotate(t);
+
+    // Left Right Case
+    // Left subtree is bigger by 1
+    // Double rotate...
+    if (balance > 1 && it > t->left->data) {
+        t->left = leftRotate(t->left);
+        return rightRotate(t);
+    }
+    
+    if (balance < -1 && it < t->right->data) {
+        t->right = rightRotate(t->right);
+        return leftRotate(t);
+    }
+
+    return t;
 }
