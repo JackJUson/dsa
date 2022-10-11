457d87c35f8875c3a6737878d9b9dd3ce91921d5
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-11 21:49:50 +1100

    Written implementation for normal BST insertion in insertAVL function

--- Trees/AVL/AVLtree.c
+++ Trees/AVL/AVLtree.c
@@ -4,3 +4,16 @@
 
 #include "AVLtree.h"
 
+Tree insertAVL(Tree t, Item it) {
+    /* 1. Perform the normal BST insertion */
+    if (t == NULL) 
+        return (newNode(it));
+
+    if (it < t->data) // Searching through left
+        t->left = insertAVL(t->left, it);
+    else if (it > t->data) // Searching through right
+        t->right = insertAVL(t->right, it);
+    else // Data already existing, tree is just returned
+        return t;
+
+}
