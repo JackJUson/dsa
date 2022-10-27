0fbc66036010056ccdeed9673ed7893a029e93ca
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-10-28 09:07:06 +1100

    Written implementation for newGraph function

--- Graphs/Graph.c
+++ Graphs/Graph.c
@@ -10,3 +10,24 @@ typedef struct GraphRep {
    int    nV;      // #vertices
    int    nE;      // #edges
 } GraphRep;
+
+Graph newGraph(int V) {
+    assert(V >= 0);
+    int i;
+
+    Graph g = malloc(sizeof(GraphRep));
+    assert(g != NULL);
+    g->nV = V;
+    g->nE = 0;
+
+    // allocate memory for each row
+    g->edges = malloc(V *sizeof(int *));
+    assert(g->edges != NULL);
+
+    // allocate memory for each column and initalise with 0
+    for (int i = 0; i < V; i++) {
+        g->edeges[i] = calloc(V, sizeof(int));
+        assert(g->edges[i] != NULL);
+    }
+    return g;
+}
