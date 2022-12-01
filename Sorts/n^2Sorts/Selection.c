b1b447deb0489af7d84f15bde262d9b416c76f89
Jackson Jung <z5397791@ad.unsw.edu.au>
2022-12-01 23:52:33 +1100

    Add documentation to Selection sort function

--- Sorts/n^2Sorts/Selection.c
+++ Sorts/n^2Sorts/Selection.c
@@ -1,4 +1,21 @@
+// Optimized implementation of Selection sort
+#include <stdio.h>
+#include <stdbool.h>
 
+void swap(int *x, int *y);
+
+// Selection Sort: Non-stable Non-Adapative = Always O(n^2)
+
+// Iterative Selection Sort
+void selectionSort(int a[], int low, int high) {
+    for (int i = low; i <= high - 1; i++) {
+        int min = i;
+        for (int j = i + 1; j <= high; j++) { 
+            if (a[j] < a[min]) min = j; // For descending order change > to <
+        }
+        swap(a[i], a[min]);
+    }
+}
 
 // Recursive Selection Sort
 // Assumption: 0 <= a <= b < size of arr
@@ -16,9 +33,7 @@ void fnB(int arr[], int a, int b) {
         return;
     }
     int i = fnA(arr, a, b);
-    int temp = arr[a];
-    arr[a] = arr[i];
-    arr[i] = temp;
+    swap(arr[a], arr[i]);
     fnB(arr, a + 1, b);
 }
 
@@ -26,15 +41,8 @@ void fn(int arr[], int size) {
     fnB(arr, 0, size - 1);
 }
 
-
-// Iterative Selection Sort
-void selectionSort(int a[], int lo, int hi) {
-   int i, j, min;
-   for (i = lo; i <= hi-1; i++) {
-      min = i;
-      for (j = i+1; j <= hi; j++) {
-         if (less(a[j],a[min])) min = j;
-      }
-      swap(a[i], a[min]);
-   }
+void swap(int *x, int *y) {
+    int temp = *x;
+    *x = *y;
+    *y = temp;
 }
